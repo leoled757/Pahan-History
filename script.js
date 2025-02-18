bd13b51320e6ae166b4e7e84bce380d249bf8680
leoled757 <mak.traher.1488.911@gmail.com>
2025-02-18 12:31:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function toggleSection(sectionId) {
     openSection = sectionId; 
   }
 }
+unction toggleMenu() {
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
